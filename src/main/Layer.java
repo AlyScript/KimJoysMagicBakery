26abb6f2b4b8d0e4ed25327bf103949c5cef8de8
AlyScript <adamaly93@gmail.com>
2024-03-08 03:26:23 +0000

    Created Stage 1 files

---
+++ src/main/Layer.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Layer {
+    private ArrayList<Ingredient> recipe;
+
+    public Layer(String name, ArrayList<Ingredient> recipe) {
+
+    }
+
+    public List<Ingredient> getRecipe() {
+
+    }
+
+    public String getRecipeDescription() {
+        
+    }
+
+}
