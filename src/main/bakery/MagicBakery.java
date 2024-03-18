cdb060f7845298ac5d9ef77b81666c318388e57d
AlyScript <adamaly93@gmail.com>
2024-03-18 02:22:00 +0000

    MagicBakery methods and some logic

--- src/main/bakery/MagicBakery.java
+++ src/main/bakery/MagicBakery.java
@@ -1,15 +1,46 @@
 package bakery;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class MagicBakery {
-    public ArrayList<Player> players;
+    public Collection<Player> players;
+    private int currentPlayerIndex;
 
     public MagicBakery(long seed, String ingredientDeckFile, String layerDeckFile) {
-        players = new ArrayList<Player>();
+        players = new LinkedList<Player>();
+        currentPlayerIndex = 0;
+    }
+
+    public boolean endTurn() {
+        if(players.isEmpty()) {
+            return false;
+        }
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+        return true;
+    }
+
+    public int getActionsPermitted() {
+        if(players.size() >= 4) {
+            return 2;
+        }
+        return 3;
+    }
+
+    public int getActionsRemaining() {
+        return getActionsPermitted() - 1;
     }
 
     public void startGame(ArrayList<String> playerNames, String customerDeckFile) {
         
+    }
+
+    public LinkedList<Player> getPlayers() {
+        return new LinkedList<>(this.players);
+    }
+
+    public Player getCurrentPlayer() {
+        return getPlayers().get(currentPlayerIndex);
     }
 
 }
